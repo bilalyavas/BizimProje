3d44f65b9da777399ba180ec2244d6fe9765fbde
bilalyavas <64182472+bilalyavas@users.noreply.github.com>
2020-04-28 19:13:54 -0400

    Update HelloWorld.java

--- src/Day_1/HelloWorld.java
+++ src/Day_1/HelloWorld.java
@@ -3,5 +3,7 @@ package Day_1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello World");
+            
     }
 }
